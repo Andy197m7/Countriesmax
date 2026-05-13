41ad6eeac3feff0b9036fb750c4720acd12d7c83
Andy197m7 <117936672+Andy197m7@users.noreply.github.com>
2026-05-13 13:52:59 -0500

    Delete backend directory

--- backend/netlify/functions/countries.js
+++
@@ -1,8 +0,0 @@
-const { getCountries, corsHeaders, respond } = require("./_data");
-
-exports.handler = async (event) => {
-  if (event.httpMethod === "OPTIONS") return { statusCode: 204, headers: corsHeaders(), body: "" };
-  try {
-    return respond(200, getCountries().sort((a, b) => a.name.localeCompare(b.name)));
-  } catch (err) { return respond(500, { error: err.message }); }
-};
